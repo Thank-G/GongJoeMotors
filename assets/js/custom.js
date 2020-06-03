130f9256e50d6730cf5db658e994cc2a6cce77cc
Thank-G <praisejahwithmetb@gmail.com>
2020-06-03 22:24:32 +0100

    contact functionality and links connection

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -38,16 +38,16 @@ $(document).ready(function() {
         
         
         //Do not include! This prevents the form from submitting for DEMO purposes only!
-        $('form').submit(function(event) {
-            event.preventDefault();
-            return false;
-        })
+        // $('form').submit(function(event) {
+        //     event.preventDefault();
+        //     return false;
+        // })
     });
 
     // Header Type = Fixed
     $(window).on("scroll", function() {
         var window_top = $(window).scrollTop() + 1;
-        if (window_top > 300) {
+        if (window_top > 50) {
             $('.site-header').addClass('scrolled-header animated fadeInDown');
         } else {
             $('.site-header').removeClass('scrolled-header animated fadeInDown');
@@ -261,6 +261,33 @@ $(document).ready(function() {
       title: "Menu",
       format: "multitoggle"
     });
+
+
+    //  go to top
+    var offset = 1000,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 500,
+		//grab the "back to top" link
+		$back_to_top = $('.go-top');
+
+		//hide or show the "back to top" link
+		$(window).on('scroll', function(){
+			( $(this).scrollTop() > offset ) ? $back_to_top.addClass('go-top-visible') : $back_to_top.removeClass('go-top-visible go-top-fade-out');
+			if( $(this).scrollTop() > offset_opacity ) { 
+				$back_to_top.addClass('go-top-fade-out');
+			}
+		});
+
+		//smooth scroll to top
+		$back_to_top.on('click', function(event){
+			event.preventDefault();
+			$('body,html').animate({
+				scrollTop: 0 ,
+			 	}, scroll_top_duration
+			);
+		});
 
 });
 
